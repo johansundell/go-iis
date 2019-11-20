b3f2b2640403c1ae532ba43533c3092aca8f9f1c
Johan Sundell <johansundell@gmail.com>
2019-11-20 15:11:48 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	port := "8080"
+	if os.Getenv("ASPNETCORE_PORT") != "" {
+		port = os.Getenv("ASPNETCORE_PORT")
+	}
+
+	srv := &http.Server{
+		//Handler: MainHandler,
+		Addr: ":" + port,
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	http.HandleFunc("/", MainHandler)
+
+	log.Println(srv.ListenAndServe())
+}
+
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello 2")
+}
